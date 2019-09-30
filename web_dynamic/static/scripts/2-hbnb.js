d959a7b48819e1f984fa5a4c120e76262903d4c0
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-09-30 01:44:20 +0000

    Adding jQUERY for place data - Working on Owner info with getUser function

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -15,8 +15,8 @@ $(document).ready(function () {
   });
 
   const url = 'http://0.0.0.0:5001/api/v1/status/';
-  $.get(url, function (data, status) {
-    if (status === 'success') {
+  $.get(url, function (data) {
+    if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
       $('DIV#api_status').removeClass('available');
